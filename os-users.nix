5aa9359b873574ffbf582e83ba7cb22a79d33728
Dambaev Alexander <ice.redmine@gmail.com>
2021-08-15 03:23:47 +0800

    os-users.nix: add user mike

--- os-users.nix
+++ os-users.nix
@@ -2,6 +2,11 @@
 # users.users value
 { config, pkgs, ... }:
 {
+  mike = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAranSbjHrgfa5ScJZuMLV9UQxfau0Gv2pyPEU6Mpl+pAz1yCCB0cur+18ne+8PrKaf9AIxEdBtYXNeGTGpQtBAfEaiwQ61qrTMEpSZZgE1SJk5iXoY7zBnTMisvZeauY4XnfRoyQC1bsFDFHFBvfxfJs0llDhXoWrQRjWMAmRnuPBRHRJB7QoIAjw4NZJzKE8hxmsg3u1NqLU2hR/fAkVMD8PZZVlhiLTGS5N4U2rp5lCmjf8WLVaMiags5qRg4Q3SRrhx7cqv2aMr8FY1+TqDcYJXVD1NHzk+V6aHgxkEm2QQDLq5nEXNKkMtjmLGb+GAOrEL1zatZJwSwuLREsj+PJHZXpSRxGICknSiA6Gl4pjfcm97/I5K5bO31lOmfRFU4WJhCDkGqtpme5B9dOycZvsuGMD6lH9J/XF/MYUCNDC+t7PaV0ppFIceRsAHbtirq2Cq/sAyKh0xqYwMoCTJuiEX9WlzxcIxAtQH4al+IMIN4sBCC/AldOqvaleyRXw7q5PduSm2xFS08QzXLjv3mkmx3mpnubTHpa6XgnYinoGzn2hOaq6qqsNbGI7ML6Cl9F/GzfGm5oPvCX8zYYr4UtUB7/pW0eBN8Zyjc8oub9YbvrXNA2CzUg6eUt4YEjMbjUoe0LD5aTs2AcjfaLpqK7DMI1fL2b1UY8sd0rqXkk= elmer-mike-prgmr" ];
+    };
   thartman = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
