abbaf56b3657df93f95f5ff6af9a71c5a54a54be
Dambaev Alexander <ice.redmine@gmail.com>
2021-08-15 02:21:57 +0800

    configuration.nix: provide blocks_dir value

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,7 @@
     enable = true;
     db_dir = "/data/electrs_db"; # /data/ is a separate volume
     cookie_file = "/data/bitcoin-mempool/.cookie";
+    blocks_dir = "/data/bitcoin-mempool/blocks";
   };
 
   services.bitcoind.mempool = {
